0141c8ac390ed6021faac5cda554b34f5d831b23
dagonC <dagonc4@gmail.com>
2018-05-17 21:27:13 +0200

    client: updating api client and depenceny definition files

--- maas-client-tty/swagger_client/api/__init__.py
+++ maas-client-tty/swagger_client/api/__init__.py
@@ -5,4 +5,5 @@ from __future__ import absolute_import
 # import apis into api package
 from swagger_client.api.auth_api import AuthApi
 from swagger_client.api.items_api import ItemsApi
+from swagger_client.api.service_api import ServiceApi
 from swagger_client.api.users_api import UsersApi
