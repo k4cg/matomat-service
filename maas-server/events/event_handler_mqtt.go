014adde0ad74490ad0c310fe5e81a967a8a5d3bb
dagonC <dagonc4@gmail.com>
2019-05-02 22:38:43 +0200

    Adding ability to send total consumed message on topic ... SORRY adding this is an evil hack, abusing the concept but required to implement the desired behavior with minimal change. If anybody feels like it, please feel free to improve!

--- maas-server/events/event_handler_mqtt.go
+++ maas-server/events/event_handler_mqtt.go
@@ -28,6 +28,31 @@ func NewEventHandlerMqtt(connectionString string, clientID string, topic string,
 }
 
 func (eh *EventHandlerMqtt) ItemConsumed(userID uint32, username string, itemID uint32, itemName string, itemCost int32, count uint32) {
+	eh.publishMessage(eh.buildItemConsumedMessage(userID, username, itemID, itemName, itemCost, count))
+}
+
+func (eh *EventHandlerMqtt) buildItemConsumedMessage(userID uint32, username string, itemID uint32, itemName string, itemCost int32, count uint32) string {
+	//TODO - shouldn't the username used instead of the user ID?
+	//TODO - the int casts are messy ... improve!
+	message := "matomat;item-consumed;" + strconv.Itoa(int(userID)) + ";" + strconv.Itoa(int(itemID)) + ";" + itemName + ";" + strconv.Itoa(int(itemCost)) + ";" + strconv.Itoa(int(count)) + ";" + strconv.FormatInt(time.Now().Unix(), 10) //TODO implement proper message format
+	return message
+}
+
+//SORRY adding the following is an evil hack, abusing the concept
+// but required to implement the desired behavior with minimal change
+//If anybody feels like it, please feel free to improve!
+func (eh *EventHandlerMqtt) TotalItemConsumedForUserChanged(userID uint32, username string, itemID uint32, itemName string, itemCost int32, totalCount uint32) {
+	eh.publishMessage(eh.buildTotalItemConsumedForUserChanged(userID, username, itemID, itemName, itemCost, totalCount))
+}
+
+func (eh *EventHandlerMqtt) buildTotalItemConsumedForUserChanged(userID uint32, username string, itemID uint32, itemName string, itemCost int32, totalCount uint32) string {
+	//TODO - shouldn't the username used instead of the user ID?
+	//TODO - the int casts are messy ... improve!
+	message := "matomat;total-item-consumed-for-user-changed;" + strconv.Itoa(int(userID)) + ";" + strconv.Itoa(int(itemID)) + ";" + itemName + ";" + strconv.Itoa(int(itemCost)) + ";" + strconv.Itoa(int(totalCount)) + ";" + strconv.FormatInt(time.Now().Unix(), 10) //TODO implement proper message format
+	return message
+}
+
+func (eh *EventHandlerMqtt) publishMessage(message string) {
 	client := MQTT.NewClient(eh.clientOpts)
 	// connect to the broker using the mqtt client
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
@@ -36,16 +61,9 @@ func (eh *EventHandlerMqtt) ItemConsumed(userID uint32, username string, itemID 
 			log.Print("EventHandlerMqtt: error trying to connect to MQTT broker")
 		}
 	} else {
-		token := client.Publish(eh.topic, 0, eh.retainMessage, eh.buildItemConsumedMessage(userID, username, itemID, itemName, itemCost, count))
+		token := client.Publish(eh.topic, 0, eh.retainMessage, message)
 		//wait for receipt from broker
 		token.Wait()
 		client.Disconnect(250)
 	}
-}
-
-func (eh *EventHandlerMqtt) buildItemConsumedMessage(userID uint32, username string, itemID uint32, itemName string, itemCost int32, count uint32) string {
-	//TODO - shouldn't the username used instead of the user ID?
-	//TODO - the int casts are messy ... improve!
-	message := "matomat;item-consumed;" + strconv.Itoa(int(userID)) + ";" + strconv.Itoa(int(itemID)) + ";" + itemName + ";" + strconv.Itoa(int(itemCost)) + ";" + strconv.Itoa(int(count)) + ";" + strconv.FormatInt(time.Now().Unix(), 10) //TODO implement proper message format
-	return message
 }
